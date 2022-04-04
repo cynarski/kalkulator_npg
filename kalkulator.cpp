1b7b750a9c66675100f624f3cb37ba423d7670ac
MateuszCierpik <102189453+MateuszCierpik@users.noreply.github.com>
2022-04-04 18:48:26 +0200

    Create kalkulator.cpp

---
+++ kalkulator.cpp
@@ -0,0 +1,23 @@
+#include "kalkulator.hpp"
+
+#include <cmath>
+
+#include <iostream>
+
+Wynik_dzialania::Wynik_dzialania(double a, double b, double c) {
+    pierwszy_skladnik = a;
+    drugi_skladnik = b;
+    wynik = c;
+}
+
+void Wynik_dzialania::dodawanie() {
+    std::cin >> pierwszy_skladnik;
+    std::cout << "+" << " ";
+    std::cin >> drugi_skladnik;
+
+    wynik = pierwszy_skladnik + drugi_skladnik;
+
+
+
+    std::cout << "= "<< wynik << std::endl;
+}
