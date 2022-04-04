274eba6f720f3706554b114c80faf21f43db5e4c
MateuszCierpik <102189453+MateuszCierpik@users.noreply.github.com>
2022-04-04 18:46:43 +0200

    Update kalkulator_main.cpp

--- kalkulator_main.cpp
+++ kalkulator_main.cpp
@@ -1,2 +1,16 @@
-
+#include "kalkulator.hpp"
 #include <iostream>
+
+
+int main() {
+    char wybrane_dzialanie;
+    std::cout << "wybierz dzialanie" << std::endl;
+    std::cin >> wybrane_dzialanie;
+
+    if(wybrane_dzialanie == '+'){
+        Wynik_dzialania d1(1,1,1);
+        d1.dodawanie();
+    }
+
+    return EXIT_SUCCESS;
+}
