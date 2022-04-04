c6812b15f3aeed075990daf6c70f1bc772deec7d
tymson20 <102227018+tymson20@users.noreply.github.com>
2022-04-04 19:37:11 +0200

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,8 +1,30 @@
 import math
 
+def zadanie(dane):
+    x = int(dane[0])
+    dzialanie = dane[1]
+    y = int(dane[2])
+    if dzialanie == "+":
+        print(x + y)
+    elif dzialanie == "-":
+        print(x - y)
+    return None
+
+
 def main():
+    pamiec = []
+    while True:
+        x = input("PODAJ POLECENIE, NAPISZ STOP ABY ZAKOŃCZYC: ")
+        if x == "STOP":
+            print("ZAKOŃCZONO PROGRAM")
+            break
+        else:
+            pamiec.append(x)
+            dane = x.split()
+            zadanie(dane)
 
-
+            #print(dane)
+    return None
 
 
 main()
